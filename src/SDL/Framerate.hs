a2873a59169de226d9d1dd10e2f7f26f7c39b8bf
sbidin <sinisa@bidin.cc>
2015-04-15 21:16:06 +0200

    Add higher-level bindings to framerate.h.

--- src/SDL/Framerate.hs
+++ src/SDL/Framerate.hs
@@ -1,1 +1,99 @@
-module SDL.Framerate where
+{-|
+
+Module      : SDL.Framerate
+Copyright   : (c) 2015 Siniša Biđin
+License     : MIT
+Maintainer  : sinisa@bidin.cc
+Stability   : experimental
+
+Bindings to @SDL2_gfx@'s framerate management functionality. These actions
+should allow you to set, manage and query a target application framerate.
+
+There are two ways to use this functionality:
+
+* Simply wrap your render loop using 'with', or
+* create a 'Manager', 'set' a 'Framerate' and call 'delay' manually.
+
+-}
+
+module SDL.Framerate
+  ( Framerate
+  , Manager(..)
+  , with
+  , with_
+  , set
+  , delay
+  , minimum
+  , maximum
+  , get
+  , count
+  ) where
+
+import Control.Monad          (void)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Data.Typeable          (Typeable)
+import Foreign.Marshal.Alloc  (alloca)
+import Foreign.Ptr            (Ptr)
+import Prelude         hiding (minimum, maximum)
+
+import qualified SDL.Raw.Framerate
+
+-- | A framerate manager, counting frames and keeping track of time delays
+-- necessary to reach a certain target framerate.
+newtype Manager = Manager (Ptr SDL.Raw.Framerate.Manager)
+  deriving (Eq, Typeable)
+
+-- | A certain number of frames per second.
+type Framerate = Int
+
+-- | Create a new framerate 'Manager' using the default settings.
+manager :: MonadIO m => m Manager
+manager =
+  fmap Manager . liftIO . alloca $ \ptr -> do
+    SDL.Raw.Framerate.init ptr
+    return ptr
+
+-- | The smallest allowed framerate.
+minimum :: Framerate
+minimum = SDL.Raw.Framerate.FPS_LOWER_LIMIT
+
+-- | The largest allowed framerate.
+maximum :: Framerate
+maximum = SDL.Raw.Framerate.FPS_UPPER_LIMIT
+
+-- | Set a target framerate and reset delay interpolation. Note that the given
+-- framerate must be within the allowed range -- otherwise the minimum or
+-- maximum allowed framerate is used instead.
+set :: MonadIO m => Manager -> Framerate -> m ()
+set (Manager ptr) = void . set' . min maximum . max minimum
+  where
+    set' = SDL.Raw.Framerate.setFramerate ptr . fromIntegral
+
+-- | Get the currently set framerate.
+get :: MonadIO m => Manager -> m Framerate
+get (Manager ptr) = fmap fromIntegral $ SDL.Raw.Framerate.getFramerate ptr
+
+-- | Returns the framecount. Each time 'delay' is called, a frame is counted.
+count :: MonadIO m => Manager -> m Int
+count (Manager ptr) = fmap fromIntegral $ SDL.Raw.Framerate.getFramecount ptr
+
+-- | Generate and apply a delay in order to maintain a constant target
+-- framerate. This should be called once per rendering loop. Delay will
+-- automatically be set to zero if the computer cannot keep up (if rendering is
+-- too slow). Returns the number of milliseconds since the last time 'delay'
+-- was called (possibly zero).
+delay :: MonadIO m => Manager -> m Int
+delay (Manager ptr) = fmap fromIntegral $ SDL.Raw.Framerate.framerateDelay ptr
+
+-- | Given a target 'Framerate' and initial state, wraps a render loop and
+-- automatically calls 'delay' each frame. You still get access to the
+-- 'Manager' if you want to change things dynamically. Each iteration's return
+-- state is passed as input to the next iteration.
+with :: MonadIO m => Framerate -> s -> (Manager -> s -> m s) -> m ()
+with fps st render = manager >>= \m -> set m fps >> loop m st
+  where
+    loop m st' = render m st' >>= \st'' -> delay m >> loop m st''
+
+-- | Same as 'with', but doesn't give access to the 'Manager'.
+with_ :: MonadIO m => Framerate -> s -> (s -> m s) -> m ()
+with_ fps st = with fps st . const
