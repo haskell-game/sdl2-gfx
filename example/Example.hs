fb83e1d44629499a80b202845e2f2564d1882023
sbidin <sinisa@bidin.cc>
2015-04-15 23:44:36 +0200

    Add simple example.

--- example/Example.hs
+++ example/Example.hs
@@ -2,6 +2,8 @@
 
 module Main where
 
+import Control.Monad (when)
+
 import qualified SDL
 import qualified SDL.Framerate
 
@@ -12,7 +14,17 @@ main = do
   w <- SDL.createWindow "sdl2-gfx-example" SDL.defaultWindow
   SDL.showWindow w
 
-  -- TODO: Example.
+  fpsm <- SDL.Framerate.manager
+  SDL.Framerate.set fpsm 25 -- Run at 25 frames per second.
+
+  -- Draw bunch of stuff, repeat for 100 frames.
+  -- If everything OK, should run 100/25=4 seconds.
+  let loop = do
+        frames <- SDL.Framerate.count fpsm
+        _ <- SDL.Framerate.delay fpsm
+        when (frames < 100) loop
+
+  loop
 
   SDL.destroyWindow w
   SDL.quit
