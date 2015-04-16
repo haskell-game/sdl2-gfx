9cbeef8dabb61d1779c558438ca2fd51072051d2
sbidin <sinisa@bidin.cc>
2015-04-17 00:27:19 +0200

    Simplify Raw.Helper by not generating signature.

--- src/SDL/Raw/Helper.hs
+++ src/SDL/Raw/Helper.hs
@@ -31,22 +31,19 @@ liftF fname cname ftype = do
   let f' = mkName $ fname ++ "'" -- Direct binding.
   let f  = mkName fname          -- Lifted.
   t' <- ftype                    -- Type of direct binding.
-  t  <- liftToMonadIO t'         -- Type of lifted binding.
 
   -- The generated function accepts n arguments.
   args <- replicateM (countArgs t') $ newName "x"
 
   return
-    [
-      PragmaD $ InlineP f Inline FunLike AllPhases
-    , SigD f t
+    [ ForeignD $ ImportF CCall Unsafe cname f' t'
+    , PragmaD $ InlineP f Inline FunLike AllPhases
     , FunD f
         [ Clause
             (map VarP args)
             (NormalB $ 'liftIO `applyTo` [f' `applyTo` map VarE args])
             []
         ]
-    , ForeignD $ ImportF CCall Unsafe cname f' t'
     ]
 
 -- | How many arguments does a function of a given type take?
@@ -58,24 +55,6 @@ countArgs = count 0
       (ForallT _ _ t) -> count n t
       (SigT t _)      -> count n t
       _               -> n
-
--- | Changes a function's type to return a MonadIO action instead of an IO one.
-liftToMonadIO :: Type -> Q Type
-liftToMonadIO t' = do
-  m <- newName "m"
-  return $ ForallT
-    [PlainTV m]
-    [AppT (ConT ''MonadIO) (VarT m)]
-    (replaceLast m t')
-
--- | Replaces the last type constructor to a given one.
-replaceLast :: Name -> Type -> Type
-replaceLast m = \case
-  (ForallT a b t)            -> ForallT a b $ replaceLast m t
-  (SigT t k)                 -> SigT (replaceLast m t) k
-  (AppT a@(AppT ArrowT _) t) -> AppT a $ replaceLast m t
-  (AppT (ConT _) r)          -> AppT (VarT m) r
-  _                          -> error "SDL.Raw.Helper.liftF: I goofed up."
 
 -- | An expression where f is applied to n arguments.
 applyTo :: Name -> [Exp] -> Exp
