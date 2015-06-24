e448524654a441d4a220d2405f02f65728da7c43
sbidin <sinisa@bidin.eu>
2015-06-25 00:39:58 +0200

    Add an ExceptionHelper module.

--- src/SDL/Primitive.hs
+++ src/SDL/Primitive.hs
@@ -74,7 +74,7 @@ import Data.Word              (Word8)
 import Foreign.C.Types        (CInt)
 import Linear                 (V4(..), V2(..))
 import Prelude         hiding (length)
-import SDL.Exception          (throwIfNeg_)
+import SDL.ExceptionHelper    (throwIfNeg_)
 import SDL.Internal.Types     (Renderer(..))
 
 import qualified SDL.Raw.Primitive
